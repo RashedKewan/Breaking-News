dc189d390b15f2058fa830a779a65079f42e9f7e
Rashed kewan <kewan.rashed@gmail.com>
2022-11-15 11:37:19 +0200

    adding index file

--- src/main/java/com/spring/ynetbreakingnews/YnetNewsController.java
+++ src/main/java/com/spring/ynetbreakingnews/YnetNewsController.java
@@ -1,9 +1,11 @@
-package com.springboot.ynetnews_;
+package com.spring.ynetbreakingnews;
+
 
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
